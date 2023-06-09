8cbe7ba0822c6e85dec7fff48a2746f0b1e47c3d
Pedro Zuppelli <pedro.zuppelli@gmail.com>
2023-06-09 14:33:01 -0300

    Adds front end app

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,32 @@ const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   gender: { type: String, required: true },
   likedSongs: { type: [String], default: [] },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {_id: this.id, name: this.name},
+    process.env.JWTPRIVATEKEY,
+    {expiresIn: '6d'}
+  )
+
+  return token;
+}
+
+const validate = (user) => {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(10).required(),
+    email: Joi.string().email.required(),
+    password: passwordComplexity().required()
+  });
+
+  return schema.validate(user);
+}
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = {user, valdate};
